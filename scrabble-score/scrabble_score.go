412796132e7fb4aa0cab635b25cafd0a6f4ee6cc
Hubert Woszczyk <woszczykh@gmail.com>
2021-02-15 19:02:44 +0100

    Final solution for scrabble_score

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -1,42 +1,45 @@
-// Package scrabble provides a scoring function
+// Package scrabble provides a scoring function for English Scrabble.
 package scrabble
 
-var points = [256]byte{
+import "strings"
+
+// The size is 128 because we use the ASCII value of a letter as the index and 'z' is 122 in ASCII.
+var points = [128]int{
 	' ': 0,
-	'A': 1, 'a': 1,
-	'E': 1, 'e': 1,
-	'I': 1, 'i': 1,
-	'O': 1, 'o': 1,
-	'U': 1, 'u': 1,
-	'L': 1, 'l': 1,
-	'N': 1, 'n': 1,
-	'R': 1, 'r': 1,
-	'S': 1, 's': 1,
-	'T': 1, 't': 1,
-	'D': 2, 'd': 2,
-	'G': 2, 'g': 2,
-	'B': 3, 'b': 3,
-	'C': 3, 'c': 3,
-	'M': 3, 'm': 3,
-	'P': 3, 'p': 3,
-	'F': 4, 'f': 4,
-	'H': 4, 'h': 4,
-	'V': 4, 'v': 4,
-	'W': 4, 'w': 4,
-	'Y': 4, 'y': 4,
-	'K': 5, 'k': 5,
-	'J': 8, 'j': 8,
-	'X': 8, 'x': 8,
-	'Q': 10, 'q': 10,
-	'Z': 10, 'z': 10,
+	'a': 1,
+	'e': 1,
+	'i': 1,
+	'o': 1,
+	'u': 1,
+	'l': 1,
+	'n': 1,
+	'r': 1,
+	's': 1,
+	't': 1,
+	'd': 2,
+	'g': 2,
+	'b': 3,
+	'c': 3,
+	'm': 3,
+	'p': 3,
+	'f': 4,
+	'h': 4,
+	'v': 4,
+	'w': 4,
+	'y': 4,
+	'k': 5,
+	'j': 8,
+	'x': 8,
+	'q': 10,
+	'z': 10,
 }
 
 // Score returns the number of points a word is worth, according to English
 // Scrabble.
 func Score(word string) int {
 	var score int
-	for i := 0; i < len(word); i++ {
-		score += int(points[word[i]])
+	for _, r := range strings.ToLower(word) {
+		score += points[r]
 	}
 	return score
 }
