3861be84a896b6486f4ff270fd831a2c7e13a175
Hubert Woszczyk <woszczykh@gmail.com>
2021-02-21 12:06:40 +0100

    Simplified IsIsogram's solution by using bool

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -7,17 +7,17 @@ import (
 
 // IsIsogram returns true if a string is an isogram.
 func IsIsogram(s string) bool {
-	occurences := map[rune]int{}
+	occurences := map[rune]bool{}
 
 	for _, r := range strings.ToLower(s) {
 		if !unicode.IsLetter(r) {
 			continue
 		}
 
-		if occurences[r] > 0 {
+		if occurences[r] {
 			return false
 		}
-		occurences[r]++
+		occurences[r] = true
 	}
 
 	return true
